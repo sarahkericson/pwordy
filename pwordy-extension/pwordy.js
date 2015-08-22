ef56e193e9a01e0225ff331237ecf0405534b877
Sarah Ericson <sarahkericson@gmail.com>
2015-08-21 21:29:08 -0700

    Pwordy initial commit

---
+++ pwordy-extension/pwordy.js
@@ -0,0 +1,20 @@
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)]
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var pword = words.randomElement() +
+              words.randomElement() +
+              words.randomElement() +
+              words.randomElement()
+
+  var pwordElem = document.getElementById("pword")
+  pwordElem.innerText = pword
+
+  document.getElementById('button').addEventListener('click', function(e) {
+    var range = document.createRange();  
+    range.selectNode(pwordElem);
+    window.getSelection().addRange(range);
+    document.execCommand('copy');  
+  });
+})
