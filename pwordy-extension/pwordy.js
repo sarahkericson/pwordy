e8ef68483e1f4a8a2cedbcc82cc40cb432263e5b
Sarah Ericson <sarahkericson@gmail.com>
2015-08-21 21:46:23 -0700

    Add character count

--- pwordy-extension/pwordy.js
+++ pwordy-extension/pwordy.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   var pwordElem = document.getElementById("pword")
   pwordElem.innerText = pword
+  document.getElementById("pword-length").innerText = pword.length + " characters"
 
   document.getElementById('button').addEventListener('click', function(e) {
     var range = document.createRange();  
