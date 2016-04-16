6745f8ac98194c719af188f97f98614edf2a2b72
Sarah Ericson <sarahkericson@gmail.com>
2016-04-16 15:09:46 -0700

    Style updates - Change fonts - Modifiable input box - Better button

--- pwordy-extension/pwordy.js
+++ pwordy-extension/pwordy.js
@@ -1,21 +1,36 @@
 Array.prototype.randomElement = function () {
     return this[Math.floor(Math.random() * this.length)]
-}
+};
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  var pwordElem = document.getElementById("pword");
+  var pwordDataElem = document.getElementById("pword-data");
+  var buttonElem = document.getElementById("button");
+
+  pwordElem.addEventListener('input', function(e) {
+    // Update pword data text
+    pwordDataElem.innerText = pwordElem.value.length + " characters";
+
+    // Button to uncopied state
+    buttonElem.classList.remove("copied");
+  });
+
+  buttonElem.addEventListener('click', function(e) {
+    // Copy pword to clipboard
+    // http://stackoverflow.com/questions/13899299/write-text-to-clipboard
+    pwordElem.select();
+    document.execCommand("Copy", false, null);
+    window.getSelection().removeAllRanges();
+
+    // Button to copied state
+    buttonElem.classList.add("copied");
+  });
+
   var pword = words.randomElement() +
               words.randomElement() +
               words.randomElement() +
               words.randomElement()
 
-  var pwordElem = document.getElementById("pword")
-  pwordElem.innerText = pword
-  document.getElementById("pword-length").innerText = pword.length + " characters"
-
-  document.getElementById('button').addEventListener('click', function(e) {
-    var range = document.createRange();  
-    range.selectNode(pwordElem);
-    window.getSelection().addRange(range);
-    document.execCommand('copy');  
-  });
-})
+  pwordElem.value = pword
+  pwordElem.dispatchEvent(new Event('input'));
+});
